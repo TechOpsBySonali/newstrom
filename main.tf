653bddb144aa191885cda21d47e6c2845f8cc940
sonali kurade <sonali.k593@gmail.com>
2025-12-28 18:44:45 +0530

    Initial commit: Add Terraform configuration for Azure Resource Group

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Configure the Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
